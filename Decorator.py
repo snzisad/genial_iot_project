891ef5db3ffab115cba39eb9bc27d1227d5f9a49
snzisad <snzisad@gmail.com>
2021-12-09 00:08:29 +0100

    jwt integrated

---
+++ Decorator.py
@@ -0,0 +1,64 @@
+import jwt
+from functools import wraps
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from dotenv import dotenv_values
+
+x_access_token = dotenv_values(".env")["HEADER_KEY"]
+secret_key = dotenv_values(".env")["SECRET_KEY"]
+
+
+app = Flask(__name__)
+client = MongoClient(port=27017)
+db = client["IOT_PROJECT"]
+
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+  
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, secret_key)
+            current_user = db.user.find_one({"_id": data['public_id']})
+
+            # returns the current logged in users contex to the routes
+            return  f(current_user, *args, **kwargs)
+
+        except Exception as e:
+            print(e)
+            
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+  
+    return decorated
+  
+
+# decorator for verifying the request
+def verify_request(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+            
+        if not token:
+            return jsonify({'message' : 'Bad Request'}), 401
+  
+        if token == x_access_token:
+            return  f(*args, **kwargs)
+        else:
+            return jsonify({
+                'message' : 'Bad Request'
+            }), 401
+  
+    return decorated
+  
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, jsonify, request, send_from_directory, make_response
-import pandas as pd
-import json
-from pymongo import MongoClient, results
+import uuid
+import jwt
+from flask import Flask, jsonify, request, send_from_directory
+from pymongo import MongoClient
 from dotenv import dotenv_values
 from uuid import uuid4
 from pprint import pprint
+from datetime import datetime, timedelta
+from  werkzeug.security import generate_password_hash, check_password_hash
+from Decorator import verify_request, token_required
 
 
 app = Flask(__name__)
@@ -23,21 +26,13 @@ def send_static_file(file):
 
 @app.route("/")
 def index():
-    
-    # db.rooms.find({'_id':1}, {'$set':{'name': "ZISAD"}})
-    # db.rooms.delete_many({}) # delete room data
-    # db.room_sensor.delete_many({}) #delete room information from room sensor
-    # db.sensor_data.delete_many({}) #delete all previous data of this room
-    
+    # db.user.delete_many({})
 
-    # db.sensors.delete_many({})
-
-    # db.sensors.insert_many([
-    #     {'_id': 0, 'name': "Temperature"},
-    #     {'_id': 1, 'name': "Pressure"},
-    #     {'_id': 2, 'name': "Sound"},
-    #     {'_id': 3, 'name': "Humidity"},
-    # ])
+    # db.user.insert_one({
+    #     "_id": str(uuid4()),
+    #     "username": "admin",
+    #     "password": generate_password_hash("123456")
+    # })
 
     # make_response(
     #         'Could not verify',
@@ -49,345 +44,342 @@ def index():
     # pass
 
 
-@app.route('/api/sensor_list', methods=["GET"])
-def sensor_list():
-    
-    if(validateAPIRequest(request)):
-        all_data = db.sensors.find()
-        
-        return jsonify({
-            'status': True,
-            'data': [data for data in all_data],
-        }), 200
+@app.route('/api/login', methods=["POST"])
+@verify_request
+def login():
+    request_data = request.form
+    username = request_data.get('username')
+    password = request_data.get('password')
 
-    else:
+    if username is None:
         return jsonify({
             'status': False,
-            'message' : 'Bad Request'
-        }), 403
+            'error_at': 'username',
+            'message': 'Username is required'
+        }), 422
+
+    if password is None:
+        return jsonify({
+            'status': False,
+            'error_at': 'password',
+            'message': 'Password is required'
+        }), 422
+
+    user = db.user.find_one({"username": username})
+
+    
+    if user is not None and check_password_hash(user["password"], password):
+        # generates the JWT Token
+        token = jwt.encode({
+            'public_id': str(user["_id"]),
+            'exp' : datetime.utcnow() + timedelta(minutes = 30)
+        }, app.config['SECRET_KEY'])
+  
+        return jsonify({
+            'status': True,
+            'token' : token.decode('UTF-8')
+        }), 200
+
+
+    return jsonify({
+        'status': False,
+        'message': 'Invalid username or password'
+    }), 403
+
+    
+@app.route('/api/sensor_list', methods=["GET"])
+@token_required
+def sensor_list(*_):
+    data = db.sensors.find()
+    
+    return jsonify({
+        'status': True,
+        'data': list(data),
+    }), 200
 
     
 
 
 @app.route('/api/add_sensor', methods=["POST"])
-def add_sensor():
+@token_required
+def add_sensor(*_):
+    name = request.form.get('name')
     
-    if(validateAPIRequest(request)):
-        try:
-            name = request.form['name']
-        except :
-            return jsonify({
-                'status': False,
-                'message': 'Sensor name is required'
-            }), 422
-
-        last_item = db.sensors.find_one({}, sort = [('_id', -1)])
-        if last_item == None:
-            id = 0
-        else:
-            id = last_item['_id']+1
-
-        
-        db.sensors.insert_one(
-            {'_id': id, 'name': name},
-        )
-        
-        return jsonify({
-            'status': True,
-            'message': 'Sensor added successfully'
-        }), 200
-
-    else:
+    if name is None:
         return jsonify({
             'status': False,
-            'message' : 'Bad Request'
-        }), 403
+            'error_at': 'name',
+            'message': 'Sensor name is required'
+        }), 422
+
+    last_item = db.sensors.find_one({}, sort = [('_id', -1)])
+    if last_item == None:
+        id = 0
+    else:
+        id = last_item['_id']+1
+
+    
+    db.sensors.insert_one(
+        {'_id': id, 'name': name},
+    )
+    
+    return jsonify({
+        'status': True,
+        'message': 'Sensor added successfully'
+    }), 200
 
     
 
 @app.route('/api/delete_sensor', methods=["DELETE"])
-def delete_sensor():
+@token_required
+def delete_sensor(*_):
+    id = request.form.get('id')
     
-    if(validateAPIRequest(request)):
-        try:
-            id = int(request.form['id'])
-        except :
-            return jsonify({
-                'status': False,
-                'message': 'Sensor id is required'
-            }), 422
-        
-        db.sensors.delete_one({'_id': id}) #delete sensor data
-        db.room_sensor.delete_many({'sensor_id': id}) #delete sensor information from room sensor
-        db.sensor_data.delete_many({'sensor_id': id}) #delete all previous data of this sensor
-        
-        return jsonify({
-            'status': True,
-            'message': 'Sensor information removed successfully'
-        }), 200
-
-    else:
+    if id is None :
         return jsonify({
             'status': False,
-            'message' : 'Bad Request'
-        }), 403
+            'error_at': 'id',
+            'message': 'Sensor id is required'
+        }), 422
+    
+    try:
+        id = int(id)
+    except Exception as e:
+        print(e)
+        return jsonify({
+            'status': False,
+            'error_at': 'id',
+            'message': "Room id should be integer value"
+        }), 422
+
+
+    db.sensors.delete_one({'_id': id}) #delete sensor data
+    db.room_sensor.delete_many({'sensor_id': id}) #delete sensor information from room sensor
+    db.sensor_data.delete_many({'sensor_id': id}) #delete all previous data of this sensor
+    
+    return jsonify({
+        'status': True,
+        'message': 'Sensor information removed successfully'
+    }), 200
 
 
 @app.route('/api/room_list', methods=["GET"])
-def room_list():
-    
-    if(validateAPIRequest(request)):
-        rooms = db.rooms.find()
+@token_required
+def room_list(*_):
+    rooms = db.rooms.find()
         
-        return jsonify({
-            'status': True,
-            'data': [data for data in rooms],
-        }), 200
-
-    else:
-        return jsonify({
-            'status': False,
-            'message' : 'Bad Request'
-        }), 403
+    return jsonify({
+        'status': True,
+        'data': [data for data in rooms],
+    }), 200
 
     
 
 @app.route('/api/room_sensor_list', methods=["GET"])
-def room_sensor_list():
-    
-    if(validateAPIRequest(request)):
-
-        pipeline = [
-            {
-                "$lookup": {
-                    'from': "room_sensor",
-                    'localField': '_id',
-                    'foreignField': 'room_id',
-                    'as': "room_sensor_list",
-                    'pipeline':[
-                        {
-                            "$lookup": {
-                                'from': "sensors",
-                                'as': "sensor_info",
-                                "let": { "room_sensor_id": "$sensor_id" },
-                                "pipeline": [
-                                    { 
-                                        "$match": { 
-                                            "$expr": { 
-                                                "$eq": ["$_id", "$$room_sensor_id"] 
-                                            }
+@token_required
+def room_sensor_list(*_):
+    pipeline = [
+        {
+            "$lookup": {
+                'from': "room_sensor",
+                'localField': '_id',
+                'foreignField': 'room_id',
+                'as': "room_sensor_list",
+                'pipeline':[
+                    {
+                        "$lookup": {
+                            'from': "sensors",
+                            'as': "sensor_info",
+                            "let": { "room_sensor_id": "$sensor_id" },
+                            "pipeline": [
+                                { 
+                                    "$match": { 
+                                        "$expr": { 
+                                            "$eq": ["$_id", "$$room_sensor_id"] 
                                         }
                                     }
-                                ],
-                            }
+                                }
+                            ],
                         }
-                    ],
-
-                }
-            },
-            {
-            "$unwind": {
-                "path": "$room_sensor_list",
-                "preserveNullAndEmptyArrays": True
-                }
-            },
-            {
-                "$group": {
-                    "_id": "$_id",
-                    "name": {
-                        "$first": "$name"
-                    },
-                    "sensor": {
-                        "$push": "$room_sensor_list.sensor_info"
-
                     }
+                ],
+
+            }
+        },
+        {
+        "$unwind": {
+            "path": "$room_sensor_list",
+            "preserveNullAndEmptyArrays": True
+            }
+        },
+        {
+            "$group": {
+                "_id": "$_id",
+                "name": {
+                    "$first": "$name"
+                },
+                "sensor": {
+                    "$push": "$room_sensor_list.sensor_info"
+
                 }
-            },
-            {
-                "$sort": {
-                    "_id": -1
-                }
-            },
-        ]
+            }
+        },
+        {
+            "$sort": {
+                "_id": -1
+            }
+        },
+    ]
 
-        data = db.rooms.explain("executionStats").aggregate(pipeline)
-        
-        return jsonify({
-            'status': True,
-            'data': list(data),
-        }), 200
-
-
-    else:
-        return jsonify({
-            'status': False,
-            'message' : 'Bad Request'
-        }), 403
+    data = db.rooms.aggregate(pipeline)
+    
+    return jsonify({
+        'status': True,
+        'data': list(data),
+    }), 200
 
 
 @app.route('/api/add_room', methods=["POST"])
-def add_room():
+@token_required
+def add_room(*_):
+    name = request.form.get('name')
     
-    if(validateAPIRequest(request)):
-        try:
-            name = request.form['name']
-        except :
-            return jsonify({
-                'status': False,
-                'message': 'Room name is required'
-            }), 422
-
-        sensor_ids = request.form.getlist('sensor_ids[]')
-        if len(sensor_ids) == 0:
-            return jsonify({
-                'status': False,
-                'message': 'Sensor array is required'
-            }), 422
-
-        last_item = db.rooms.find_one({}, sort = [('_id', -1)])
-        if last_item == None:
-            id = 0
-        else:
-            id = last_item['_id']+1
-
-        
-        db.rooms.insert_one(
-            {'_id': id, 'name': name},
-        )
-        
-        room_sensor_data = []
-        for sensor_id in sensor_ids:
-            room_sensor_data.append({'_id': uuid4(), 'room_id': id, 'sensor_id': int(sensor_id)})
-        
-        db.room_sensor.insert_many(room_sensor_data)
-        
-        return jsonify({
-            'status': True,
-            'message': 'Room added successfully'
-        }), 200
-
-    else:
+    if name is None :
         return jsonify({
             'status': False,
-            'message' : 'Bad Request'
-        }), 403
+            'error_at': 'name',
+            'message': 'Room name is required'
+        }), 422
+
+    sensor_ids = request.form.getlist('sensor_ids[]')
+    if len(sensor_ids) == 0:
+        return jsonify({
+            'status': False,
+            'error_at': 'sensor_ids',
+            'message': 'Sensor array is required'
+        }), 422
+
+    last_item = db.rooms.find_one({}, sort = [('_id', -1)])
+    if last_item == None:
+        id = 0
+    else:
+        id = last_item['_id']+1
+
+    
+    db.rooms.insert_one(
+        {'_id': id, 'name': name},
+    )
+    
+    room_sensor_data = []
+    for sensor_id in sensor_ids:
+        try:
+            room_sensor_data.append({'_id': uuid4(), 'room_id': id, 'sensor_id': int(sensor_id)})
+        except:
+            return jsonify({
+                'status': False,
+                'error_at': 'sensor_ids',
+                'message': 'Sensor array elements must be integer'
+            }), 200
+
+    db.room_sensor.insert_many(room_sensor_data)
+    
+    return jsonify({
+        'status': True,
+        'message': 'Room added successfully'
+    }), 200
 
 
 @app.route('/api/update_room', methods=["PUT"])
-def update_room():
+@token_required
+def update_room(*_):
+    name = request.form.get('name')
+    id = request.form.get('id')
     
-    if(validateAPIRequest(request)):
-        try:
-            name = request.form['name']
-        except :
-            return jsonify({
-                'status': False,
-                'message': 'Room name is required'
-            }), 422
-        try:
-            id = int(request.form['id'])
-        except :
-            return jsonify({
-                'status': False,
-                'message': 'Room id is required'
-            }), 422
-
-        sensor_ids = request.form.getlist('sensor_ids[]')
-        if len(sensor_ids) == 0:
-            return jsonify({
-                'status': False,
-                'message': 'Sensor array is required'
-            }), 422
-
-        db.rooms.update_one({'_id': id}, {'$set':{'name': name}})
-
-        db.room_sensor.delete_many({'room_id': id}) #delete room information from room sensor
-        db.sensor_data.delete_many({'room_id': id}) #delete all previous data of this room
-
-        room_sensor_data = []
-        for sensor_id in sensor_ids:
-            room_sensor_data.append({'_id': uuid4(), 'room_id': id, 'sensor_id': int(sensor_id)})
-        
-        db.room_sensor.insert_many(room_sensor_data)
-        
-        return jsonify({
-            'status': True,
-            'message': 'Room updated successfully',
-        }), 200
-
-    else:
+    if name is None :
         return jsonify({
             'status': False,
-            'message' : 'Bad Request'
-        }), 403
+            'message': 'Room name is required'
+        }), 422
+    
+    if id is None:
+        return jsonify({
+            'status': False,
+            'message': 'Room id is required'
+        }), 422
+
+    try:
+        id = int(id)
+    except Exception as e:
+        print(e)
+        return jsonify({
+            'status': False,
+            'error_at': 'id',
+            'message': "Room id should be integer value"
+        }), 422
+
+    sensor_ids = request.form.getlist('sensor_ids[]')
+    if len(sensor_ids) == 0:
+        return jsonify({
+            'status': False,
+            'message': 'Sensor array is required'
+        }), 422
+
+    db.rooms.update_one({'_id': id}, {'$set':{'name': name}})
+
+    db.room_sensor.delete_many({'room_id': id}) #delete room information from room sensor
+    db.sensor_data.delete_many({'room_id': id}) #delete all previous data of this room
+
+    room_sensor_data = []
+    for sensor_id in sensor_ids:
+        try:
+            room_sensor_data.append({'_id': uuid4(), 'room_id': id, 'sensor_id': int(sensor_id)})
+        except:
+            return jsonify({
+                'status': False,
+                'error_at': 'sensor_ids',
+                'message': 'Sensor array elements must be integer'
+            }), 200
+
+    
+    db.room_sensor.insert_many(room_sensor_data)
+    
+    return jsonify({
+        'status': True,
+        'message': 'Room updated successfully',
+    }), 200
 
     
 
 @app.route('/api/delete_room', methods=["DELETE"])
-def delete_room():
+@token_required
+def delete_room(*_):
+    id = request.form.get('id')
     
-    if(validateAPIRequest(request)):
-        try:
-            id = int(request.form['id'])
-        except :
-            return jsonify({
-                'status': False,
-                'message': 'Room id is required'
-            }), 422
-        
-        db.rooms.delete_many({'_id': id}) # delete room data
-        db.room_sensor.delete_many({'room_id': id}) #delete room information from room sensor
-        db.sensor_data.delete_many({'room_id': id}) #delete all previous data of this room
-        
-        return jsonify({
-            'status': True,
-            'message': 'Room information removed successfully'
-        }), 200
-
-    else:
+    if id is None:
         return jsonify({
             'status': False,
-            'message' : 'Bad Request'
-        }), 403
+            'message': 'Room id is required'
+        }), 422
 
-
-    
-@app.route('/api/search', methods=["POST"])
-def search():
-    
-    if(validateAPIRequest(request)):
-        temp = request.form['temp']
-        pressure = request.form['pressure']
-        sound = request.form['sound']
-
-        data = [1, 2, 3, 4]
-        
-        return jsonify({
-            'status': True,
-            'data' : data
-        }), 200
-
-    else:
-        return jsonify({
-            'status': False,
-            'message' : 'Bad Request'
-        }), 403
-
-    
-
-
-def validateAPIRequest(request):
     try:
-        token = request.headers['x_access_token']
-
-        if token == x_access_token:
-            return True
-
-        return False
-
+        id = int(id)
     except Exception as e:
         print(e)
-        return False
+        return jsonify({
+            'status': False,
+            'error_at': 'id',
+            'message': "Room id should be integer value"
+        }), 422
 
+    db.rooms.delete_many({'_id': id}) # delete room data
+    db.room_sensor.delete_many({'room_id': id}) #delete room information from room sensor
+    db.sensor_data.delete_many({'room_id': id}) #delete all previous data of this room
+    
+    return jsonify({
+        'status': True,
+        'message': 'Room information removed successfully'
+    }), 200
+
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
